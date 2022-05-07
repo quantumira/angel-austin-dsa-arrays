cec7a1887b585561e92143d08b26cbf53437e19a
quantumira <67796159+quantumira@users.noreply.github.com>
2022-05-07 19:47:53 +0000

    add filter

--- js/catgame.js
+++ js/catgame.js
@@ -11,4 +11,4 @@
 
     var Cat = function(index) {
         var _cat = {
-            div: $("<div class='tile cat'></div>").css("left", boar
+            div: $("<div class='tile cat'></div>").css("left", board.w

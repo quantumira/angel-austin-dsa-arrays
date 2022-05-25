798ef63c6f799d99070188e644c01581230ee2c5
quantumira <67796159+quantumira@users.noreply.github.com>
2022-05-25 19:47:03 +0000

    excel

--- js/catgame.js
+++ js/catgame.js
@@ -15,4 +15,4 @@
             state: randElem(feelings)
         };
         _cat.otherStates = function() {
-            return $.grep(feelings, function (
+            return $.grep(feelings, function (f) { 

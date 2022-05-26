c7a463df9355d7346a45332180fc2a96dba2df90
quantumira <67796159+quantumira@users.noreply.github.com>
2022-05-26 07:38:59 +0000

    Machine info

--- js/catgame.js
+++ js/catgame.js
@@ -15,4 +15,4 @@
             state: randElem(feelings)
         };
         _cat.otherStates = function() {
-            return $.grep(feelings, function (f) { 
+            return $.grep(feelings, function (f) { retur

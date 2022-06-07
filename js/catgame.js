dcbc9f800ca454b5bbab8efe238d9438565e893a
quantumira <67796159+quantumira@users.noreply.github.com>
2022-06-07 09:23:39 +0000

    update 1 files: biblio/biblio-ht.data

--- js/catgame.js
+++ js/catgame.js
@@ -18,4 +18,4 @@
             return $.grep(feelings, function (f) { return f != _cat.state });
         }
         _cat.div.click(function() {
-            if ((_cat.state == 'b
+            if ((_cat.state == 'bor

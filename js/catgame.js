8b168aa120531c799e02ccba2409e5d04e12fcf6
quantumira <67796159+quantumira@users.noreply.github.com>
2022-07-01 10:34:59 +0000

    make release

--- js/catgame.js
+++ js/catgame.js
@@ -22,4 +22,4 @@
                 || (_cat.state == 'hungry' && activeElem.type == 'food')
                 || (_cat.state == 'thirsty' && activeElem.type == 'water')
                 || (_cat.state == 'dirty' && activeElem.type == 'litter')) {
-                _cat.changeState(randElem(_cat.othe
+                _cat.changeState(randElem(_cat.otherState

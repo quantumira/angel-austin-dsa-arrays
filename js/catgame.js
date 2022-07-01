a9d9251dbeb132d88b85b5dff93d67961361db66
quantumira <67796159+quantumira@users.noreply.github.com>
2022-07-01 17:31:29 +0000

    removed debugging prints

--- js/catgame.js
+++ js/catgame.js
@@ -22,4 +22,5 @@
                 || (_cat.state == 'hungry' && activeElem.type == 'food')
                 || (_cat.state == 'thirsty' && activeElem.type == 'water')
                 || (_cat.state == 'dirty' && activeElem.type == 'litter')) {
-                _cat.changeState(randElem(_cat.otherStates()))
+                _cat.changeState(randElem(_cat.otherStates()));
+  

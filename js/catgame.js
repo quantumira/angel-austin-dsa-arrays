c9ae145d2ad87fd105b4536b031860cb61466342
quantumira <67796159+quantumira@users.noreply.github.com>
2022-07-30 09:21:59 +0000

    udpating readme

--- js/catgame.js
+++ js/catgame.js
@@ -28,4 +28,4 @@
             } else if (_cat.state == 'content' && activeElem.type == randElem(elemTypes)){
                 _cat.changeState('happy');
                 var happy = true;
-                for (var i=0; i<cats.len
+                for (var i=0; i<cats.length;

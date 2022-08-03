400032a0868dede370e6a84b7775b89f09601f3b
quantumira <67796159+quantumira@users.noreply.github.com>
2022-08-03 15:08:08 +0000

    bump @datawrapper/chart-core

--- js/catgame.js
+++ js/catgame.js
@@ -28,4 +28,4 @@
             } else if (_cat.state == 'content' && activeElem.type == randElem(elemTypes)){
                 _cat.changeState('happy');
                 var happy = true;
-                for (var i=0; i<cats.length;i++)
+                for (var i=0; i<cats.length;i++) {

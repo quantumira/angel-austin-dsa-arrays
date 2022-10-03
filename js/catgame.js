c64ee9c78ce1206e7f3552f205e7ea483088fd69
quantumira <67796159+quantumira@users.noreply.github.com>
2022-10-03 13:29:02 +0000

    212

--- js/catgame.js
+++ js/catgame.js
@@ -46,4 +46,4 @@
         });
         _cat.changeState = function(newState) {
             if(_cat.state != newState) {
-                console.log("Changed from " + _cat.state + ' t
+                console.log("Changed from " + _cat.state + ' to ' 

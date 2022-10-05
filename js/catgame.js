c990fddfeca6a806d723bc6157812a6378f91ec6
quantumira <67796159+quantumira@users.noreply.github.com>
2022-10-05 20:20:00 +0000

    Gemspec

--- js/catgame.js
+++ js/catgame.js
@@ -46,4 +46,6 @@
         });
         _cat.changeState = function(newState) {
             if(_cat.state != newState) {
-                console.log("Changed from " + _cat.state + ' to ' + newState)
+                console.log("Changed from " + _cat.state + ' to ' + newState);
+
+  
